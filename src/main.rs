ec21d7856d0a39298792299cf0a1194a6ed1e964
Andrei Tanana <andrei.tanana@deltatre.it>
2019-05-28 16:05:42 +0300

    post data init

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate select;
 use std::error::Error;
 
 use regex::{Captures, Regex};
-use reqwest::Response;
+use reqwest::{Client, Response};
 use select::document::Document;
 use select::predicate::Attr;
 
@@ -16,11 +16,11 @@ fn main() {
 }
 
 fn test_print() -> Result<(), Box<Error>> {
-    // let response = get_page()?;
-    // let data = parse_data(response)?;
-    let data: Vec<String> = vec![r#"<li><b><a href="/wiki/%D0%9C%D1%83%D0%BD%D0%BA,_%D0%9F%D0%B5%D1%82%D0%B5%D1%80_(%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D0%BD%D0%B8%D0%BC%D0%B0%D1%82%D0%B5%D0%BB%D1%8C)" title="Мунк, Петер (предприниматель)">Еврейский бизнесмен</a></b> торговал <a href="/wiki/%D0%A0%D0%BE%D0%B6%D0%B4%D0%B5%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D1%81%D0%BA%D0%B0%D1%8F_%D1%91%D0%BB%D0%BA%D0%B0" title="Рождественская ёлка">рождественскими ёлками</a> и подряжался развивать туризм в <a href="/wiki/%D0%AD%D0%BB%D1%8C-%D0%93%D0%B8%D0%B7%D0%B0" title="Эль-Гиза">Гизе</a>.</li>"#.to_string()];
-    let clean_data = clear_data(data);
-    println!("{:#?}", clean_data);
+    let response = get_page()?;
+    let data = parse_data(response)?;
+    let clean_data = clear_data(data)?;
+    let result = send_data(clean_data)?;
+    println!("{:#?}", result);
     Ok(())
 }
 
@@ -51,4 +51,18 @@ fn clear_line(line: &String, tag_regex: &Regex) -> String {
             _ => "".to_string()
         }
     }).to_string()
+}
+
+fn send_data(data: Vec<String>) -> reqwest::Result<Response> {
+    let client = Client::new();
+    let token = env!("BOT_TOKEN");
+    let url = format!("https://api.telegram.org/bot{}/sendMessage", token);
+    let params = [
+        ("chat_id", "@wiki_dyk"),
+        ("text", &data.join("\n\n")),
+        ("parse_mode", "HTML")
+    ];
+    let request = client.post(&url).form(&params);
+    println!("{:#?}", request);
+    request.send()
 }
